a2e9e2b7ccfbfa7f72d9297ba2177004e19c0560
Slaith12 <34562469+Slaith12@users.noreply.github.com>
2024-10-26 15:29:01 -0400

    update test code

--- src/raspi/samples/can/odrive_motor_test.py
+++ src/raspi/samples/can/odrive_motor_test.py
@@ -1,7 +1,7 @@
 import logging
 
 from component.can import can_bus as canbus_action
-from component.motor import odrive_motor_action
+from component.motor import odrive_motor_action as odrive_action
 from component.motor.odrive_enums import *
 from state_management import configure_device
 
@@ -10,9 +10,13 @@ configure_device("src/raspi/pinconfig.json")
 can_bus = "can_bus_1"
 motor = "odrive_1"
 
-odrive_motor_action.request_set_state(motor, MotorState.FULL_CALIBRATION_SEQUENCE)
+odrive_action.request_set_state(motor, MotorState.FULL_CALIBRATION_SEQUENCE)
 
-while odrive_motor_action.get_state(motor) != MotorState.IDLE:
+while odrive_action.get_state(motor) != MotorState.IDLE:
+    print(f"Current state: {odrive_action.get_state(motor)}.")
+    print(f"Current position: {odrive_action.get_current_position(motor)}.")
+    print(f"Current velocity: {odrive_action.get_current_velocity(motor)}.")
+    print("")
     pass
 
 canbus_action.close(can_bus)
