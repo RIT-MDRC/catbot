4062f90abf9d0b458c5fbd122616f98e89ecc4cf
bab8295 <bab8295@g.rit.edu>
2024-11-09 13:01:46 -0500

    made test potentiometer have a mock ADC, unit tests pass

--- src/unit_tests/raspi/component/test_potentiometer.py
+++ src/unit_tests/raspi/component/test_potentiometer.py
@@ -3,13 +3,17 @@ from component.adc import ADC_action
 from component.potentiometer import potentiometer_actions
 from state_management import *
 
+class MockADC():
+    def read_data(self,address):
+        # address is a forced parameter as the analog input device always sends its own address
+        # to the ADC to be read from when the analog input device attempts a .value call on itself,
+        # and in this case potentiometer_actions.get_degree is just a .value call on itself, therefor it needs the address param
+        return True
+
 class PotentiomenterTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         configure_logger()
-
-        # can potentially make a mock adc class,
-        # just needs a read_data function
 
         # cls.adc = ADC_action.parse_adc({
         #     "i2c": "smbus",
@@ -22,10 +26,10 @@ class PotentiomenterTest(unittest.TestCase):
         # )
         # log_states()
         cls.pot = ADC_action.parse_analog_input_device({
-            "adc": "adc",
+            "adc": MockADC(),
             "address": 0
         }, "pot1")
 
     def test_get_degree(self):
-        degree = potentiometer_actions.get_degree("adc.pot1") 
+        self.assertTrue(potentiometer_actions.get_degree(self.pot))
         
