c2abb6eede7a63eda78df50a1788c2fd4a83db89
isa6513 <isa6513@rit.edu>
2024-11-12 19:21:13 -0500

    Added more raw_motor unit tests

--- src/unit_tests/raspi/component/test_raw_motor.py
+++ src/unit_tests/raspi/component/test_raw_motor.py
@@ -2,7 +2,7 @@ import unittest
 
 from component.motor import raw_motor
 from state_management import identifier
-from state_management.utils import FakePWMOutputDevice
+from state_management.utils import FakePWMOutputDevice, FakeDigitalOutputDevice
 from component.motor.pin import direction_pin_action, speed_pin_action
 
 
@@ -52,6 +52,68 @@ class raw_motor_unit_tests(unittest.TestCase):
         results = raw_motor.get_speed(test_raw_motor)
 
         self.assertEqual(results, test_speed)
+
+
+    def test_check_direction_WhenInvalidDirection_ThrowsError(self):
+        with self.assertRaises(ValueError):
+            raw_motor.check_direction(raw_motor.RawMotor(1), 2)
+
+
+    def test_check_direction_WhenMatchingDirection_ReturnsTrue(self):
+        test_direction = 1
+        test_raw_motor = raw_motor.RawMotor(1)
+        test_raw_motor.direction = FakeDigitalOutputDevice(0, test_direction)
+
+        results = raw_motor.check_direction(test_raw_motor, test_direction)
+
+        self.assertTrue(results)
+
+
+    def test_check_direction_WhenNotMatchingDirection_ReturnsFalse(self):
+        test_direction = 1
+        test_wrong_direction = 0
+        test_raw_motor = raw_motor.RawMotor(1)
+        test_raw_motor.direction = FakeDigitalOutputDevice(0, test_direction)
+
+        results = raw_motor.check_direction(test_raw_motor, test_wrong_direction)
+
+        self.assertFalse(results)
+
+
+    def test_check_speed_WhenInvalidSpeed_ThrowsError(self):
+        with self.assertRaises(ValueError):
+            raw_motor.check_speed(raw_motor.RawMotor(1), 2)
+
+
+    def test_check_speed_WhenMatchingSpeed_ReturnsTrue(self):
+        test_speed = 1
+        test_raw_motor = raw_motor.RawMotor(1)
+        test_raw_motor.speed = FakePWMOutputDevice(0, test_speed)
+
+        results = raw_motor.check_speed(test_raw_motor, test_speed)
+
+        self.assertTrue(results)
+
+
+    def test_check_speed_WhenNotMatchingSpeed_ReturnsFalse(self):
+        test_speed = 1
+        test_wrong_speed = 0
+        test_raw_motor = raw_motor.RawMotor(1)
+        test_raw_motor.speed = FakePWMOutputDevice(0, test_speed)
+
+        results = raw_motor.check_speed(test_raw_motor, test_wrong_speed)
+
+        self.assertFalse(results)
+
+
+    def test_get_direction_Always_GetsDirection(self):
+        test_direction = 1
+        test_raw_motor = raw_motor.RawMotor(1)
+        test_raw_motor.direction = FakeDigitalOutputDevice(0, test_direction)
+
+        results = raw_motor.get_direction(test_raw_motor)
+
+        self.assertEqual(results, test_direction)
 
 
 if __name__ == "__main__":
