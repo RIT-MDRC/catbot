a74621829a7d0f785a95a10ea5ae69fd0473e70a
hirototakeuchi <hiroto125takeuchi@gmail.com>
2024-11-15 16:39:59 -0500

    feat: odrive motor control code

--- src/foundation/setup.py
+++ src/foundation/setup.py
@@ -10,6 +10,10 @@ setup(
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
         ("lib/python3.12/site-packages/foundation", ["foundation/pinconfig.json"]),
+        (
+            "lib/python3.12/site-packages/foundation/component/motor",
+            ["foundation/component/motor/odrive-cansimple.dbc"],
+        ),
     ],
     install_requires=["setuptools", "dotenv"],
     zip_safe=True,
