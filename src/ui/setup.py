3842f2ce2f61017defa0dcf7a470c87dca5b7bb2
Catbot <htmdrc@rit.edu>
2024-11-15 21:06:16 -0500

    fix: drop version to 3.10

--- src/ui/setup.py
+++ src/ui/setup.py
@@ -9,7 +9,7 @@ setup(
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
-        ("lib/python3.12/site-packages/ui", ["ui/index.tcss"]),
+        ("lib/python3.10/site-packages/ui", ["ui/index.tcss"]),
     ],
     install_requires=["setuptools", "dotenv"],
     zip_safe=True,
